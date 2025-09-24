e1fe003c684a6a3d148f6d35c8d7485293c83c4a
bv0209 <saul.de@alumnos.upm.es>
2025-09-24 20:16:30 +0200

    he puesto en app, crear un objeto de app, y he puesto metodos de inicilizar, run y cerrar, vacios todos; ademas he creado la clase Ticker, tambien vacia

--- src/main/java/upm/App.java
+++ src/main/java/upm/App.java
@@ -3,6 +3,21 @@ package upm;
 public class App {
 
     public static void main(String[] args) {
+        App app=new App();
+        app.init();
+        app.run();
+        app.close();
+    }
+
+    private void init(){
         System.out.println("Run App..");
+    }
+
+    private void run(){
+
+    }
+
+    private void close(){
+
     }
 }
