e7e2337dab5ea2f815013ed4e04f67a79746824d
julianviidal <julianrozadillasvidal@gmail.com>
2025-10-29 20:14:00 +0100

    he creado la clase cash para crear los metedos q tienen q ver con el cajero y otra handlecommand que va a hacer los diferetnes comandos de cajero

---
+++ src/main/java/upm/Handlecommand.java
@@ -0,0 +1,15 @@
+package upm;
+
+import java.util.*;
+
+public class Handlecommand {
+    private Map<String, Cash> cash;
+
+    public Handlecommand() {
+        this.cash = new HashMap<>();
+    }
+
+    private void cashadd(String[] arrayUserInput) {
+
+    }
+}
