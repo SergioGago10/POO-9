74e2069299e2afbd63a4a1cbcd288e7dba42c654
JesusMartinezL3 <jmlopeztercero@proton.me>
2025-11-09 02:57:36 +0100

    Ampliación de la clase Ticket para incluir todo lo que pone en el guión de la segunda entrega

--- src/main/java/upm/tickets/Ticket.java
+++ src/main/java/upm/tickets/Ticket.java
@@ -1,16 +1,29 @@
 package upm.tickets;
 
 import upm.Catalog;
-import upm.Products.BasicProduct;
-import upm.Products.Category;
-import upm.Products.IProduct;
+import upm.Products.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Ticket {
     private final static int MAX_PRODUCTOS = 100;
     private List<IProduct> productsList;
+    private String ticketId;
+    private int cashId;
+    private int userId;
+    private boolean closed;
+    private TicketState estado;
+    private LocalDateTime fechaCierre;
+    private List<String> customTexts; //Para saber que personalizacion tiene cada producto personalizable
 
-    public Ticket() {
+    public Ticket(String ticketId, int cashId, int userId) {
+        this.ticketId = ticketId;
+        this.cashId = cashId;
+        this.userId = userId;
+        this.closed = false;
+        this.estado = TicketState.EMPTY;
         productsList = new ArrayList<>();
         // El arraylist es util, ya que vamos a recorrer la lista siempre y la eliminación de productos será al inicio mitad y final,
         // por lo que una linked list no supone mucha diferencia, además que con tan pocos elementos no hay ninguna diferencia notoria entre
@@ -21,8 +34,22 @@ public class Ticket {
         // en esa situación sí que sería más util usar la linked list u otra estructura de datos mas avanzada (a pesar de que el acceso a elementos por indice sea mas costoso)
     }
 
+    public String getTicketId() {return ticketId;}
+    public boolean isClosed() {return closed;}
+    public int getUserId() {return userId;}
+    public int getCashId() {return cashId;}
+    public TicketState getEstado(){return estado;}
     public List<IProduct> getProductsList() {
         return Collections.unmodifiableList(productsList); //No queremos que se modifique el ticket, por lo que pasamos una copia solo para lectura
+    }
+    public List<String> getCustomTexts() {return Collections.unmodifiableList(customTexts);}
+
+    public void setCustomTexts(List<String> texts) {
+        if (texts == null) {
+            this.customTexts = new ArrayList<>();
+        } else {
+            this.customTexts = new ArrayList<>(texts); // copia defensiva
+        }
     }
 
     public void sortProducts() {
@@ -43,24 +70,85 @@ public class Ticket {
         productsList.sort(Comparator.comparing(IProduct::getName, String.CASE_INSENSITIVE_ORDER));
     }
 
-    public void addProductToTicket(int productID, int quantity) {
-        boolean productAdded = false;
-        IProduct productToBeAdded = Catalog.getProduct(productID);
-        if (productToBeAdded != null) {
-            boolean canAdd = true;
-            for (int i = 0; (i < quantity) && (canAdd); i++) {
-                if (productsList.size() >= MAX_PRODUCTOS) {
-                    System.err.println("You can't add more products to the ticket. Try to make a new one if needed.");
-                    canAdd = false;
-                } else {
-                    productsList.add(productToBeAdded);
-                    productAdded = true;
+    public void addProductToTicket(int productID, int quantity, List<String> customTexts) {
+        if(estado != TicketState.CLOSED){
+            boolean productAdded = false;
+            IProduct productToBeAdded = Catalog.getProduct(productID);
+            if (productToBeAdded != null) {
+                boolean canAdd = true;
+                for (int i = 0; (i < quantity) && (canAdd); i++) {
+                    if (productsList.size() >= MAX_PRODUCTOS) {
+                        System.out.println("You can't add more products to the ticket. Try to make a new one if needed.");
+                        canAdd = false;
+                    }
+
+                    else if (productToBeAdded instanceof FoodProduct || productToBeAdded instanceof MeetingProduct) {
+                        // No añadir reuniones/comidas repetidas
+                        boolean alreadyInTicket = false;
+                        for (IProduct p : productsList) {
+                            if (p.getId() == productID) {
+                                alreadyInTicket = true;
+                            }
+                        }
+                        if (alreadyInTicket) {
+                            System.out.println("This product (Meal/Meeting) is already in the ticket. It can not be added again.");
+                            canAdd = false;
+                        } else {
+                            productsList.add(productToBeAdded);
+                            productAdded = true;
+                        }
+                    }
+
+                    else if (productToBeAdded instanceof CustomizableProduct) {
+                        CustomizableProduct customProduct = (CustomizableProduct) productToBeAdded;
+                        List<String> textsToAdd;
+                        if (customTexts == null) {
+                            // No hay textos personalizados, usamos una lista vacía
+                            textsToAdd = new ArrayList<>();
+                        } else {
+                            // Copiamos la lista de textos pasada
+                            textsToAdd = new ArrayList<>(customTexts);
+                        }
+                        if (textsToAdd.size() > customProduct.getMaxCustomTexts()) {
+                            System.out.println("Too many custom texts for this product. Max allowed: " + customProduct.getMaxCustomTexts());
+                            canAdd = false;
+                        }  else {
+                            // Creamos la copia del producto con precio actualizado
+                            CustomizableProduct copy = new CustomizableProduct(
+                                    customProduct.getId(),
+                                    customProduct.getName(),
+                                    customProduct.getCategory(),
+                                    customProduct.getPrice(),
+                                    customProduct.getMaxCustomTexts()
+                            );
+                            // Guardamos los textos personalizados en el copy
+                            copy.setCustomTexts(textsToAdd);
+                            double finalPrice = copy.calculateFinalPrice();
+                            copy.setPrice(finalPrice);
+
+                            productsList.add(copy);
+                            productAdded = true;
+                        }
+                    }
+
+                    else { // Producto normal (BasicProduct)
+                        productsList.add(productToBeAdded);
+                        productAdded = true;
+                    }
                 }
+                if (productAdded) {
+                    //Imprimimos el ticket actual despues de poner los productos
+                    if(estado != TicketState.ACTIVE){
+                        estado = TicketState.ACTIVE;
+                    }
+                    printCurrentTicket();
+                    System.out.println("ticket add: ok");
+                }
+            } else{
+                System.out.println("Product not found in catalog.");
             }
-            if (productAdded) {
-                printCurrentTicket();
-                System.out.println("ticket add: ok");
-            }
+        }else {
+            System.out.println("This ticket has been closed. You can't add or remove products from it.");
         }
     }
 
@@ -101,13 +189,38 @@ public class Ticket {
         }
     }
 
+    public void printFinalTicket() {
+        if(estado != TicketState.EMPTY){
+            closeTicket();
+        }
+        printCurrentTicket();
+    }
+
     public void removeProductFromTicket(int productID) {
-        Iterator<IProduct> it = productsList.iterator();
-        while (it.hasNext()) {
-            IProduct p = it.next();
-            if (p.getId() == productID) {
-                it.remove();
+        if(estado != TicketState.CLOSED){
+            Iterator<IProduct> it = productsList.iterator();
+            while (it.hasNext()) {
+                IProduct p = it.next();
+                if (p.getId() == productID) {
+                    it.remove();
+                }
             }
+            if(productsList.isEmpty()){
+                estado = TicketState.EMPTY;
+            }
+        } else {
+            System.out.println("This ticket has been closed. You can't add or remove products from it.");
         }
     }
+
+    public void closeTicket(){
+        if (estado != TicketState.CLOSED){
+            estado = TicketState.CLOSED;
+            fechaCierre = LocalDateTime.now();
+            // Añadir fecha de cierre al ID
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd-HH:mm");
+            ticketId = ticketId + "-" + fechaCierre.format(formatter);
+        }
+    }
+
 }
---
+++ src/main/java/upm/tickets/TicketState.java
@@ -0,0 +1,5 @@
+package upm.tickets;
+
+public enum TicketState {
+    EMPTY,ACTIVE,CLOSED
+}
