d5463899627b783b5ea607e417068367ee7506ba
JesusMartinezL3 <jmlopeztercero@proton.me>
2025-11-15 11:46:07 +0100

    TicketMAnager ahora maneja todos los tickets en un map, y ademas maneja los ticketsbycashid, por otro mapa, para poder imprimir y tener guardados los tickets por id del cajero, el ticketmanager al ser la clase que maneja todos los tickets tambien maneja los del propio cajero, si en un futuro se ponen mas tipos podemos hacer una clase abstracta para especializaciones y demas.

--- src/main/java/upm/tickets/TicketManager.java
+++ src/main/java/upm/tickets/TicketManager.java
@@ -6,12 +6,14 @@ import java.util.*;
 
 public class TicketManager {
     // Map para acceder rápido a los tickets por su ID
-    private Map<String, Ticket> tickets;
+    private Map<String, Ticket> ticketsByTicketId;
+    private Map<Integer,List<Ticket>> ticketsByCashId;
+
     public TicketManager() {
-        tickets = new HashMap<>();
+        ticketsByTicketId = new HashMap<>();
     }
 
-    public boolean exists(String ticketId) {return tickets.containsKey(ticketId);}
+    public boolean exists(String ticketId) {return ticketsByTicketId.containsKey(ticketId);}
 
     private String generateTicketId() {
         LocalDateTime now = LocalDateTime.now();
@@ -19,44 +21,77 @@ public class TicketManager {
         String datePart = now.format(fmt);
         Random rand = new Random();
         int num = rand.nextInt(100000); // [0 - 99999]
-        String fiveDigits = String.format("%05d", num); // obligamos que el numero sea de 5 digitos
-        return datePart + String.format("%05d", Integer.parseInt(fiveDigits));
+        String fiveDigits = String.format("%05d", num);
+        return datePart + fiveDigits;
     }
 
+    //Guardamos el ticket en el ticketmanager y en el ticketCashier.
     public Ticket newTicket(int cashId, int userId) {
         String ticketId = generateTicketId();
         while (exists(ticketId)) { //En caso de que exista ya esa clave (bastante raro)
             ticketId = generateTicketId();
         }
-        Ticket ticket = new Ticket(ticketId, cashId, userId);
-        tickets.put(ticketId, ticket);
-        return ticket;
+        return newTicket(ticketId,cashId,userId);
     }
     public Ticket newTicket(String ticketId, int cashId, int userId) {
        //El ticket ya tendrá el id valido, si no pues el handler se ocupará de ello.
         Ticket ticket = new Ticket(ticketId, cashId, userId);
-        tickets.put(ticketId, ticket);
+        ticketsByTicketId.put(ticketId, ticket);
+        List<Ticket> list = ticketsByCashId.get(cashId);
+        if (list == null) { //si es null significa que no hay ningun cashier con ese id y que tenga tickets.
+            list = new ArrayList<>();
+            ticketsByCashId.put(cashId, list);
+        }
+        list.add(ticket);
         return ticket;
     }
 
     public Ticket getTicketId(String ticketId) {
-        return tickets.get(ticketId);
+        return ticketsByTicketId.get(ticketId);
     }
 
+    //false - no se encontro el id del ticket o no existe
+    //true - eliminado correctamente
     public boolean removeTicket(String ticketId) {
-        return tickets.remove(ticketId) != null;
+        Ticket ticket = ticketsByTicketId.get(ticketId);
+        if (ticket == null) return false;
+
+        List<Ticket> list = ticketsByCashId.get(ticket.getCashId());
+        if (list != null) {
+            list.remove(ticket);
+            if (list.isEmpty()) {
+                ticketsByCashId.remove(ticket.getCashId());
+            }
+        }
+        ticketsByTicketId.remove(ticketId);
+        return true;
     }
 
     public void printListTickets() {
         System.out.println("Ticket list (ordered by cashID):");
         // Convertimos el map a lista y ordenamos por cashId
-        List<Ticket> ticketList = new ArrayList<>(tickets.values());
+        List<Ticket> ticketList = new ArrayList<>(ticketsByTicketId.values());
         ticketList.sort(Comparator.comparingInt(Ticket::getCashId));
         for (Ticket t : ticketList) {
             System.out.println("{class: Ticket, ticketId: " + t.getTicketId() +
                     ", cashId: " + t.getCashId() +
                     ", userId: " + t.getUserId() +
                     ", closed: " + t.isClosed() + "}");
+        }
+    }
+
+    public void printTicketsByCashier(int cashId) {
+        List<Ticket> list = ticketsByCashId.get(cashId);
+        if (list == null || list.isEmpty()) {
+            System.out.println("Cashier " + cashId + " has no tickets.");
+        } else{
+            list.sort(Comparator.comparing(Ticket::getCashId)); //Ordenamos por cashId
+            System.out.println("Tickets for cashier " + cashId + ":");
+            for (Ticket t : list) {
+                System.out.println("{ticketId: " + t.getTicketId() +
+                        ", userId: " + t.getUserId() +
+                        ", closed: " + t.isClosed() + "}");
+            }
         }
     }
 }
