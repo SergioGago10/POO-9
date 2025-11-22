313f1d9539661506b5d98bd799163873450e5871
JesusMartinezL3 <jmlopeztercero@proton.me>
2025-11-22 15:32:51 +0100

    Creacion de la clase TicketCommands, que se encarga de mirar todos los comandos y mirar cual es el que se ha pasado para su ejecucion

---
+++ src/main/java/upm/Commands/TicketCommands.java
@@ -0,0 +1,34 @@
+package upm.Commands;
+
+import upm.tickets.TicketManager;
+import java.util.ArrayList;
+
+public class TicketCommands extends Command {
+    private final ArrayList<Command> ticketcommands = new ArrayList<>();
+    protected final TicketManager ticketManager;
+
+    public TicketCommands(String text, TicketManager ticketManager) {
+        super(text);
+        this.ticketManager = ticketManager;
+        ticketcommands.add(new TicketNewCommand(ticketManager));
+        ticketcommands.add(new TicketAddCommand(ticketManager));
+        ticketcommands.add(new TicketRemoveCommand(ticketManager));
+        ticketcommands.add(new TicketPrintCommand(ticketManager));
+        ticketcommands.add(new TicketListCommand(ticketManager));
+    }
+
+    @Override
+    public boolean apply(String[] message) {
+        boolean applied = false;
+        if (message.length > 0 && message[0].equalsIgnoreCase(text) && message.length>=2) {
+            String subCommand = message[1];
+            for (Command cmd : ticketcommands) {
+                if (!applied && cmd.getText().equalsIgnoreCase(subCommand)) {
+                    applied = cmd.apply(message);
+                }
+            }
+        }
+        return applied;
+    }
+
+}
