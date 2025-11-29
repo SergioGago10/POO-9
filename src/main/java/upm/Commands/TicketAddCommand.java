c88d823687e13b7d693705241663dc7a6eea2d82
JesusMartinezL3 <jmlopeztercero@proton.me>
2025-11-29 15:56:12 +0100

    Modificación del comando tickedaddcommand, para que sea mas sencillo de leer y se entienda todo mejor.

--- src/main/java/upm/Commands/TicketAddCommand.java
+++ src/main/java/upm/Commands/TicketAddCommand.java
@@ -14,56 +14,59 @@ public class TicketAddCommand extends Command {
 
     @Override
     public boolean apply(String[] args) {
-        //todo hacer que el comando sea menos denso
-        boolean applied = false;
         if (args.length < 6) {
             System.err.println("ticket add <ticketId> <cashId> <prodId> <amount> [--p<txt> --p<txt>]");
-        } else {
-            try {
-                String ticketId = args[2];
-                int cashId = Integer.parseInt(args[3].substring(2));
-                int prodId = Integer.parseInt(args[4]);
-                int amount = Integer.parseInt(args[5]);
-                if (Catalog.idExists(prodId)) {
-                    Ticket ticketAModificar = TicketManager.getTicketById(ticketId);
-                    if(ticketAModificar != null){
-                        if(ticketAModificar.getCashId() != cashId){
-                            System.err.println("Error: Ticket " + ticketId + " does not belong to cashier " + cashId);
-                        } else{
-                            if(args.length == 6){ //producto sin personalizaciones
-                                ticketAModificar.addProductToTicket(prodId,amount,null);
-                                applied = true;
-                            } else{
-                                ArrayList<String> customTexts = new ArrayList<>();
-                                boolean correctFormat = true;
-                                for (int i = 6; (i < args.length) && (correctFormat); i++) {
-                                    String s = args[i];
-                                    if (!s.startsWith("--p")) {
-                                        System.err.println("Error: [--p<txt>] is the correct usage, try again." + s);
-                                        correctFormat = false;
-                                    }
-                                    if (correctFormat){
-                                        customTexts.add(s.substring(3));
-                                    }
-                                }
-                                if(correctFormat){
-                                    ticketAModificar.addProductToTicket(prodId, amount, customTexts);
-                                    applied = true;
-                                }
-                            }
-                        }
-                    } else {
-                        System.err.println("Error: Ticket "  + ticketId + " does not exist.");
-                    }
-                } else {
-                    System.err.println("prodId must be an id contained in the catalog. Type 'prod list' to see all the catalog.");
-                }
-            } catch (NumberFormatException e) {
-                System.err.println("prodId and cantidad must be integers.");
-            } catch (Exception e) {
-                System.err.println("Error adding product to ticket: " + e.getMessage());
+            return false;
+        }
+        try {
+            String ticketId = args[2];
+            int cashId = Integer.parseInt(args[3].substring(2));
+            int prodId = Integer.parseInt(args[4]);
+            int amount = Integer.parseInt(args[5]);
+
+            if (!Catalog.idExists(prodId)) {
+                System.err.println("prodId must be an id contained in the catalog. Type 'prod list' to see all the catalog.");
+                return false;
+            }
+
+            Ticket ticketAModificar = TicketManager.getTicketById(ticketId);
+
+            if(ticketAModificar == null){
+                System.err.println("Error: Ticket "  + ticketId + " does not exist.");
+                return false;
+            }
+
+            if(ticketAModificar.getCashId() != cashId){
+                System.err.println("Error: Ticket " + ticketId + " does not belong to cashier " + cashId);
+                return false;
+            }
+
+            ArrayList<String> customTexts = parseCustomizations(args);
+            ticketAModificar.addProductToTicket(prodId,amount, customTexts);
+
+        } catch (NumberFormatException e) {
+            System.err.println("prodId and cantidad must be integers.");
+        } catch (Exception e) {
+            System.err.println("Error adding product to ticket: " + e.getMessage());
+        }
+        return true;
+    }
+
+    private ArrayList<String> parseCustomizations(String[] args){
+        if(args.length<=6) return null; //Caso base, no hay productos customizados, devolvemos null.
+
+        ArrayList<String> customizations = new ArrayList<>();
+        boolean correctFormat = true;
+        for (int i = 6; i < args.length && correctFormat; i++) {
+            String s = args[i];
+            if (!s.startsWith("--p")) {
+                System.err.println("Error: expected --p<txt>, found: " + s);
+                correctFormat = false;
+            }
+            if(correctFormat){
+                customizations.add(s.substring(3));
             }
         }
-        return applied;
+        return correctFormat? customizations : null;
     }
 }
