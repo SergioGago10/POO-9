63bba488410055952d06d023f4ae1741eadd7ccb
bt0220 <sergio.gago@alumnos.upm.es>
2025-12-22 22:01:29 +0100

    Persistencia Clase CLiente(experimental)

---
+++ src/main/java/persistence/ClientDTO.java
@@ -0,0 +1,10 @@
+package persistence;
+
+
+public class ClientDTO {
+    public String name;
+    public String dni;
+    public String email;
+    public String cashId;
+    public String type; // PERSONA / COMPAÑIA
+}
---
+++ src/main/java/persistence/ClientMapper.java
@@ -0,0 +1,30 @@
+package persistence;
+
+import upm.Users.Client;
+import upm.Users.TypeClient;
+
+public class ClientMapper {
+
+    public static ClientDTO toDTO(Client client) {  //Convertir de cliente en clienteDTO
+        ClientDTO dto = new ClientDTO();
+        dto.name = client.getName();
+        dto.dni = client.getDni();
+        dto.email = client.getEmail();
+        dto.cashId = client.getCashId();
+        dto.type = client.getType().name();
+        return dto;
+    }
+
+
+
+    public static Client fromDTO(ClientDTO dto) {  //Convertir de clienteDTO a cliente cuando cargas
+        return new Client(
+                dto.name,
+                dto.dni,
+                dto.email,
+                dto.cashId,
+                TypeClient.valueOf(dto.type)
+        );
+    }
+}
+
