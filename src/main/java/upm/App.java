01d25b5fc5f524e92652e2d69472b1ff448f1b7b
JesusMartinezL3 <jmlopeztercero@proton.me>
2026-01-18 13:52:10 +0100

    Ponemos en App.java el idioma global en ingles ya que la practica lo muestra en ingles con todo, y asi forzamos que nuestro programa sea tambien en ingles

--- src/main/java/upm/App.java
+++ src/main/java/upm/App.java
@@ -11,6 +11,7 @@ import upm.json.PersistenceService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class App {
 
@@ -26,7 +27,7 @@ public class App {
     private void init() {
         // Si tu PersistenceService tiene load/read, actívalo:
         // persistence.load();
-
+        Locale.setDefault(Locale.ENGLISH); //Iniciamos y ponemos la app en inglés.
         CLI.printNextLine("Welcome to the ticket module App.");
     }
 
